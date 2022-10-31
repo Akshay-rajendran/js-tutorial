0fee9038c425f179c64dc0176b521791b7bdf0a4
Akshay <akshayaromal9@gmail.com>
2022-10-31 08:52:10 +0530

    3 types of variable declaration

---
+++ 01-variables.js
@@ -0,0 +1,11 @@
+// console.log("hello my dear")
+// var a=10;          value can be redecalre and reasign
+a=15;
+var a=15;
+console.log(a);
+// let num=77;        value can be reasign but cannot be redeclare
+num=99;
+console.log(num);
+// const c="jimbru";  values cant be changed or reasign or redeclare like var,let
+const c="jimbru"; 
+console.log(c)
