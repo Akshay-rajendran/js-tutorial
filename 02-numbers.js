3a517fb011d624188a0469b09f010163cb7b650d
Akshay <akshayaromal9@gmail.com>
2022-11-01 08:17:33 +0530

    number operation completed

---
+++ 02-numbers.js
@@ -0,0 +1,30 @@
+// console.log("bye")
+console.log(12*5)
+console.log(12+5)
+console.log(12/5)
+console.log(12-5)
+a=(10*50)*6
+console.log(a)
+let C=10
+let b=9
+let d=11
+console.log(b*C)
+console.log((b*C)/d)
+console.log(b/C)
+console.log(b+C)
+// console.log(25%5) if we do this,we will the remainder of 25/5
+console.log(67%4)
+a++
+// console.log(a++) +1 will added to the value of a
+b++
+console.log(b++)
+let num1=14
+let n=num1++
+console.log(n)
+n=++num1
+console.log(n)
+console.log(d+=15)
+console.log(d/=15)
+console.log(d*=15)
+console.log(d-=15)
+
