f5d2564004d0e61190b9235f43a16fa44eee4811
Akshay <akshayaromal9@gmail.com>
2022-11-01 09:05:04 +0530

    completed string

---
+++ 03-string.js
@@ -0,0 +1,16 @@
+let a="see "+"you"
+console.log(a)
+
+let b="come "
+let c="home,its getting late"
+let d=b+c
+console.log(d)
+
+console.log(c[3])
+let e="where are you, come home early"
+console.log(e.length)
+console.log(e[e.length-1])
+
+// let f=c.split(",")        split separete the words with what we give inside the "split("")"
+let f=e.split(" ")
+console.log(f)
